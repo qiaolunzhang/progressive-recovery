79691dc5ed167dfd362cfbfaf2c92f80347c9b6a
sid-devic <siddartha0860@gmail.com>
2018-10-24 23:34:53 -0500

    fixed bug where if sum(resource_costs) % resource @ t != 0, would return no possible configurations

--- prog.py
+++ prog.py
@@ -1,10 +1,4 @@
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
-
-def read_gml(path):
-    G = nx.read_gml(path)
-    return G
+import time
 
 # returns true if every element in an arr is zero
 def all_zeros(arr):
@@ -43,9 +37,26 @@ def possible_allocs(groups, total):
 def non_neg(recovery_costs, config):
     iteration = recovery_costs
     alloc_resources = [iteration[i] - config[i] for i in range(len(config))]
+
+    # check for the special case where the resources don't divide the sum of the costs
+    # in this case, at the final step there will be a single non-negative value (rest will be zero)
+    negative = 0
+    zeros = 0
+
     for e in alloc_resources:
         if e < 0:
-            return False
+            negative += 1
+            # return false since we shouldn't over-allocate
+        if e == 0:
+            zeros += 1
+
+    # if all our resource costs are <= 0, this is the last config in a config sequence
+    if negative + zeros == len(alloc_resources):
+        return True
+
+    # otherwise we prune this config
+    if negative > 0:
+        return False
 
     return True
 
@@ -60,8 +71,16 @@ class Node:
         self.vec = vec
         self.children = []
 
-# Returns a recursively defined tree of possible, pruned recovery configurations
+# wrapper for root recursion
 def iterate_over_failures(recovery_costs, r):
+    all_configs = iterate_over_failures_helper(recovery_costs, r)
+    root = Node(recovery_costs)
+    root.children = all_configs
+
+    return root
+
+# Returns a recursively defined tree of possible, pruned recovery configurations
+def iterate_over_failures_helper(recovery_costs, r):
     if all_zeros(recovery_costs):
         leaf = Node(-1)
         leaf.children = []
@@ -72,7 +91,8 @@ def iterate_over_failures(recovery_costs, r):
     
     # we prune the configurations to only those that don't over allocate resources
     pruned_configs = [config for config in recovery_configurations if non_neg(recovery_costs, config)]
-   
+    #print(pruned_configs)
+
     children = []
     # now descend down the recursive tree for each pruned config
     for config in pruned_configs:
@@ -80,7 +100,7 @@ def iterate_over_failures(recovery_costs, r):
         new_recovery_cost = apply_config(recovery_costs, config)
         config_node = Node(config)
         children.append(config_node)
-        config_node.children = iterate_over_failures(new_recovery_cost, r)
+        config_node.children = iterate_over_failures_helper(new_recovery_cost, r)
         #print(config_node.children)
 
     return children
@@ -88,12 +108,18 @@ def iterate_over_failures(recovery_costs, r):
 # print out all possible paths from a root to every leaf node
 # dfs with backtracking
 def root_to_leaves(root):
+    start = time.time()
     # so we don't print out root, we handle it separately
+    all_paths = []
     for child in root.children:
-        recurse(child, [], 0)
+        recurse(child, [], 0, all_paths)
+
+    end = time.time()
+    print(end - start)
+    return all_paths
 
 # recursive helper function 
-def recurse(node, path, pathLen):
+def recurse(node, path, pathLen, all_paths):
     if node.vec is -1:
         return
 
@@ -104,30 +130,17 @@ def recurse(node, path, pathLen):
     pathLen += 1
 
     if len(node.children) == 1 and node.children[0].vec == -1:
+        temp = []
         for j in range(pathLen):
-            print(newPath[j], end=' ')
-        print()
+            #print(newPath[j], end=' ')
+            temp.append(newPath[j])
+        #print()
+        # take advantage of python 'pass by reference' to add paths to our current
+        # list of all paths
+        all_paths.append(temp)
         return
 
     else:
         for child in node.children:
-            recurse(child, newPath, pathLen)
+            recurse(child, newPath, pathLen, all_paths)
         return
-
-
-def main():
-    test_costs = [5, 3, 1]
-    all_configs = iterate_over_failures(test_costs, 3)
-    root = Node(test_costs)
-    root.children = all_configs
-
-    root_to_leaves(root)
-    '''
-    G = read_gml('gml/DIGEX.gml')
-    nx.draw(G)
-    plt.draw()
-    plt.savefig('test.png')
-    '''
-
-if __name__ == "__main__":
-    main()
---
+++ recover_graph.py
@@ -0,0 +1,29 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+from prog import iterate_over_failures, root_to_leaves
+
+def read_gml(path):
+    G = nx.read_gml(path)
+    return G
+
+# given a graph G and the cost to recover a node in G (costs), 
+# we apply a recovery confiugration (config) and return the total system utility
+def simulate_recovery(G, costs, config):
+	return
+
+def main():
+    G = read_gml('gml/DIGEX.gml')
+
+    test_costs = [2, 2, 2, 2]
+    root = iterate_over_failures(test_costs, 1) 
+    all_paths = root_to_leaves(root)
+    print(all_paths)
+
+    nx.draw(G)
+    plt.draw()
+    plt.savefig('test.png')
+
+if __name__ == "__main__":
+    main()
