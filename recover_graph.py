005e3ae8e8602c683e875f124dae3d7592533744
sid-devic <siddartha0860@gmail.com>
2018-10-29 20:02:15 -0500

    recovery of a random n-node graph

--- recover_graph.py
+++ recover_graph.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 from prog import iterate_over_failures, root_to_leaves
 
@@ -9,6 +10,8 @@ def read_gml(path):
     G = nx.read_gml(path)
     return G
 
+# generate a random graph structure with len(recovery_cost) nodes
+# and some non-zero amount of edges
 def sample_graph(recovery_cost):
     G = nx.Graph()
 
@@ -19,7 +22,26 @@ def sample_graph(recovery_cost):
         G.add_node(node)
         utils.update({node: 1.0})
         rcv_amts.update({node: recovery_cost[node]})
+    
+    # generate random graph with (n-1) <= x <= n(n-1)/2
+    # n(n-1) / 2 ensures the final graph will be fully connected (+ 1 is for exclusion in python)
+    # n - 1 some arbitrary lower bound
+    n = len(recovery_cost)
+    num_edges = random.randint(n-1, (n * (n - 1)) / 2 + 1)
+    edges = 0
 
+    while edges <= num_edges:
+        # try generating a random edge
+        try:
+            G.add_edge(random.randint(0, len(recovery_cost) - 1), random.randint(0, len(recovery_cost) - 1))
+            edges += 1
+
+        # if we couldn't try again
+        except:
+            continue
+
+    '''
+    # original test graph
     G.add_edge(0,1)
     G.add_edge(0,2)
     G.add_edge(0,5)
@@ -28,10 +50,11 @@ def sample_graph(recovery_cost):
     G.add_edge(2,5)
     G.add_edge(3,4)
     G.add_edge(4,5)
-    
+    ''' 
+
     nx.set_node_attributes(G, name='util', values=utils)
     nx.set_node_attributes(G, name='rcv_amt', values=rcv_amts)
-
+    
     return G
 
 # given a graph G and the cost to recover a node in G (costs), 
@@ -60,12 +83,13 @@ def simulate_recovery(G, config):
 def main():
     G = read_gml('gml/DIGEX.gml')
 
-    test_costs = [2, 2, 2, 2, 0, 0]
+    test_costs = [5, 2, 3, 1, 0, 4]
+    #test_costs = [2, 2, 2, 2, 0, 0]
     root = iterate_over_failures(test_costs, 1)
     all_paths = root_to_leaves(root)
 
-    print(all_paths)
-    print(len(all_paths))
+    #print(all_paths)
+    #print(len(all_paths))
    
     utils = []
     G = sample_graph(test_costs)
@@ -74,7 +98,7 @@ def main():
         utils.append(simulate_recovery(G, path))
 
     print(utils)
-
+    print(max(utils))
     #nx.draw(sample_graph(test_costs))
     #plt.draw()
     #plt.savefig('test.png')
