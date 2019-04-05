a825e77cf80d66068fbe5ac4b2e1142500c5e726
Joe Ishigaki <pausch.g@gmail.com>
2019-04-05 13:08:54 -0500

    added more graph properties to show

--- RL/graph_property.py
+++ RL/graph_property.py
@@ -14,4 +14,9 @@ def calc_avg_cut_size(g):
 
 def show_graph_props(g):
     print("Diameter: ", nx.diameter(g))
+    print("Radius: ", nx.radius(g))
+    print("Density: ", nx.density(g))
     print("Avg Cut Size: ", calc_avg_cut_size(g))
+
+def retun_props(g):
+    return (nx.diameter(g),nx.radius(g),nx.density(g),calc_avg_cut_size(g))
