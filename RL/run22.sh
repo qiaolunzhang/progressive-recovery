3b1a709614313a8fba4d7519d64c0246193cf4fd
Joe Ishigaki <pausch.g@gmail.com>
2019-04-23 14:39:37 -0500

    some automation codes

---
+++ RL/run22.sh
@@ -0,0 +1,18 @@
+
+python q_progressive_recovery.py 12 0.3 0.1
+python q_progressive_recovery.py 15 0.3 0.1
+
+python q_progressive_recovery.py 12 0.3 0.3
+python q_progressive_recovery.py 15 0.3 0.3
+
+python q_progressive_recovery.py 12 0.3 0.5
+python q_progressive_recovery.py 15 0.3 0.5
+
+python q_progressive_recovery.py 12 0.1 0.1
+python q_progressive_recovery.py 15 0.1 0.1
+
+python q_progressive_recovery.py 12 0.1 0.3
+python q_progressive_recovery.py 15 0.1 0.3
+
+python q_progressive_recovery.py 12 0.1 0.5
+python q_progressive_recovery.py 15 0.1 0.5
